9cdc28d495cc215123de8bebb0107a4aec086cec
Silvia Yotovska <122317006+sisialt@users.noreply.github.com>
2023-05-21 15:17:06 +0300

    Create 04_maximal_sum

---
+++ Multidimensional lists/Exercises first/04_maximal_sum.py
@@ -0,0 +1,28 @@
+rows, cols = [int(x) for x in input().split()]
+
+matrix = []
+new_matrix = []
+
+for _ in range(rows):
+    matrix.append([int(x) for x in input().split()])
+
+max_sum = float('-inf')
+max_submatrix = []
+
+for i in range(rows - 2):
+    for j in range(cols - 2):
+        current_submatrix = [[matrix[row][col] for col in range(j, 3 + j)] for row in range(i, 3 + i)]
+        current_sum = sum([sum(inner_list) for inner_list in current_submatrix])
+
+        if max_sum < current_sum:
+            max_sum = current_sum
+            max_submatrix = current_submatrix
+
+print(f"Sum = {max_sum}")
+[print(*inner) for inner in max_submatrix]
+
+# 4 5
+# 1 5 5 2 4
+# 2 1 4 14 3
+# 3 7 11 2 8
+# 4 8 12 16 4
